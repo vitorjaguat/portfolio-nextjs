eabad3ac96f478244f46f3233b899b8e8d330b36
Vitor Butkus <vitorbutkus@gmail.com>
2023-02-09 17:06:26 -0300

    created ProjectItem component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,41 @@
+import propertyImg from '../../public/assets/projects/property.jpeg';
+import cryptoImg from '../../public/assets/projects/crypto.jpeg';
+import twitchImg from '../../public/assets/projects/twitch.jpeg';
+import netflixImg from '../../public/assets/projects/netflix.jpeg';
+
+import ProjectItem from './ProjectItem';
+
+export default function Projects() {
+  return (
+    <div className='w-full'>
+      <div className='max-w-[1240px] mx-auto px-2 py-16'>
+        <p className='text-xl tracking-widest uppercase text-[#5651e5]'>
+          Projects
+        </p>
+        <h2 className='py-4'>What I've Built</h2>
+        <div className='grid md:grid-cols-2 gap-8'>
+          <ProjectItem
+            title='Property Finder'
+            backgroundImg={propertyImg}
+            projectUrl='/property'
+          />
+          <ProjectItem
+            title='Crypto App'
+            backgroundImg={cryptoImg}
+            projectUrl='/crypto'
+          />
+          <ProjectItem
+            title='Twitch Clone'
+            backgroundImg={twitchImg}
+            projectUrl='/twitch'
+          />
+          <ProjectItem
+            title='Netflix Clone'
+            backgroundImg={netflixImg}
+            projectUrl='/netflix'
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -31,13 +31,13 @@ export default function Skills() {
               <div className='m-auto'>
                 <Image
                   alt='my skills'
-                  src='/../public/assets/skills/html.png'
+                  src='/../public/assets/skills/css.png'
                   width={64}
                   height={64}
                 />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
+                <h3>CSS</h3>
               </div>
             </div>
           </div>
@@ -47,13 +47,13 @@ export default function Skills() {
               <div className='m-auto'>
                 <Image
                   alt='my skills'
-                  src='/../public/assets/skills/html.png'
+                  src='/../public/assets/skills/javascript.png'
                   width={64}
                   height={64}
                 />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
+                <h3>JavaScript</h3>
               </div>
             </div>
           </div>
@@ -63,13 +63,13 @@ export default function Skills() {
               <div className='m-auto'>
                 <Image
                   alt='my skills'
-                  src='/../public/assets/skills/html.png'
+                  src='/../public/assets/skills/react.png'
                   width={64}
                   height={64}
                 />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
+                <h3>React</h3>
               </div>
             </div>
           </div>
@@ -79,13 +79,13 @@ export default function Skills() {
               <div className='m-auto'>
                 <Image
                   alt='my skills'
-                  src='/../public/assets/skills/html.png'
+                  src='/../public/assets/skills/tailwind.png'
                   width={64}
                   height={64}
                 />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
+                <h3>Tailwind</h3>
               </div>
             </div>
           </div>
@@ -95,13 +95,13 @@ export default function Skills() {
               <div className='m-auto'>
                 <Image
                   alt='my skills'
-                  src='/../public/assets/skills/html.png'
+                  src='/../public/assets/skills/firebase.png'
                   width={64}
                   height={64}
                 />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
+                <h3>Firebase</h3>
               </div>
             </div>
           </div>
@@ -111,13 +111,13 @@ export default function Skills() {
               <div className='m-auto'>
                 <Image
                   alt='my skills'
-                  src='/../public/assets/skills/html.png'
+                  src='/../public/assets/skills/github1.png'
                   width={64}
                   height={64}
                 />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
+                <h3>Github</h3>
               </div>
             </div>
           </div>
@@ -127,13 +127,13 @@ export default function Skills() {
               <div className='m-auto'>
                 <Image
                   alt='my skills'
-                  src='/../public/assets/skills/html.png'
+                  src='/../public/assets/skills/shopify.png'
                   width={64}
                   height={64}
                 />
               </div>
               <div className='flex flex-col items-center justify-center'>
-                <h3>HTML</h3>
+                <h3>Shopify</h3>
               </div>
             </div>
           </div>
